8c1bb1aefa0606741d26fa1dc66558bd058b9648
kirtigaur11 <92977037+kirtigaur11@users.noreply.github.com>
2021-11-21 00:16:18 +0530

    Dealer By Search UI Changes

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,41 +3,19 @@ import logo from '../img/Dr.jpg';
 
 function AboutUs() {
     return (
-        <div style={{ padding: '5%' }} className="anim altApp aboutDiv">
+        <div style={{ padding: '2%' }} className="anim altApp aboutDiv">
             <h2>WHAT MAKES US DIFFERENT?</h2>
             <div className="flex-container">
-                <div style={{ flex: '10%', margin: '3%', padding: '10px' }}>
-                    <img style={{ borderRadius: '50%' }} src={logo} alt="logo" />
+                <div style={{ flex: '10%', padding: '5px' }}>
+                    <img style={{ borderRadius: '50%',width:"250px" }} src={logo} alt="logo" />
                 </div>
-                <div style={{ flex: '20%', margin: '3%', padding: '10px', textAlign: 'left' }}>
+                <div style={{ flex: '20%', padding: '5px', textAlign: 'left' }}>
                     <ul>
                         <li><h3><span>Trusted</span> Mechanics</h3></li>
                         <li><h3><span>Expert</span> mechanics for your every need</h3></li>
                         <li><h3><span>Best-in-class</span> workmanship</h3></li>
                         <li><h3>Top of the line equipment for the <span>best service</span> experience</h3></li>
                     </ul>
-                </div>
-            </div>
-            <div className="flex-container">
-                <div className="divInfo">
-                    <h1>24 x 7</h1>
-                    <p>Service</p>
-                </div>
-                <div className="divInfo">
-                    <h1>45682+</h1>
-                    <p>Client Served</p>
-                </div>
-                <div className="divInfo">
-                    <h1>25+</h1>
-                    <p>Service Stations</p>
-                </div>
-                <div className="divInfo">
-                    <h1>1236+</h1>
-                    <p>Vehicle Serviced</p>
-                </div>
-                <div className="divInfo">
-                    <h1>31+</h1>
-                    <p>Auto-Serviced Everyday</p>
                 </div>
             </div>
         </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import SOS from './SOS';
+import DealerSearch from './DealerSearch';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 // import logo from '../img/service.jpg';
 function Home() {
+
+    const [showInfo, setShowInfo] = useState(false);
+    const [showHome, setShowHome] = useState(false);
+    let [txtName, setTxtName] = useState("");
+    interface dealersList{
+        label:string;
+        value: string
+    }
+
+    /**
+     * this method willl
+     * @param name -
+     * @returns 
+     */
+    function handleShow() : void{
+        setShowInfo(false);
+        setShowHome(true);
+      }
+
+      const dealers : dealersList = [
+        { label: 'ABC', value: 1 },
+        { label: 'XYZ', value: 2 },];
+
     return (
         <div id="home" className="homeDiv altApp anim">
             <table>
@@ -17,11 +44,58 @@ function Home() {
                 </tr>
                 <tr>
                     <td>
-                        <input type="number" id="mNum" name="mNum" placeholder="Enter your Mobile No." />
-                        <Button  size="lg" variant="primary">Send OTP</Button>
+                    <div className="flex-container" >
+                        <div><Autocomplete disablePortal  style={{backgroundColor: 'gray'}} options={dealers} autoHighlight sx={{ width: 300 }} renderInput={(params) => <TextField {...params} label="Vehicle Type" />} /></div>
+                        <div><Autocomplete disablePortal  style={{backgroundColor: 'gray'}} options={dealers} autoHighlight sx={{ width: 300 }} renderInput={(params) => <TextField {...params} label="Dealer Name" />} /></div>
+                        <div><Autocomplete disablePortal  style={{backgroundColor: 'gray'}} options={dealers} autoHighlight sx={{ width: 300 }} renderInput={(params) => <TextField {...params} label="Services" />} /></div>
+                        <div><Button size="lg" style={{margin:'2%'}} variant="primary" onClick={() => setShowInfo(!showInfo)}>SEARCH</Button></div>
+                    </div>
+                        {/* <input type="number" id="mNum" name="mNum" placeholder="Enter your Mobile No." /> */}
                     </td>
                 </tr>
             </table>
+            <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={showInfo} onHide={() => setShowInfo(!showInfo)} backdrop="static" keyboard={false}>
+                <Modal.Header closeButton>Dealer Information</Modal.Header>
+                <Modal.Body>
+                    <div className="divModal">
+                        {/* <div>NAME & E-MAIL ID -</div>
+                        <input type="text" id="fName" name="fName" onChange={(e) => setTxtName(e.target.value)} placeholder="Full Name" />
+                        <input type="text" id="eMailId" name="eMailId" placeholder="E-Mail" />
+                    </div>
+                    <div className="divModal">
+                        <div>ADDRESS -</div>                        
+                        <input type="text" id="houseNo" name="houseNo" placeholder="House No." />
+                        <input type="text" id="street" name="street" placeholder="Street" />
+                        <input type="text" id="location" name="location" placeholder="Location/City" disabled />
+                        <input type="text" id="state" name="state" placeholder="State" /> */}
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button size="sm" variant="primary" 
+                    // onClick={() => handleShow()}
+                    >BOOK NOW</Button>
+                </Modal.Footer>
+            </Modal>            
+            <Modal size="lg"  fullscreen={true} aria-labelledby="contained-modal-title-vcenter" centered show={showHome} onHide={() => setShowHome(!showHome)} backdrop="static" keyboard={false}>
+                <Modal.Header closeButton>Hi {txtName} ..!</Modal.Header>
+                <Modal.Body>
+                    <div className="divModal">
+                        <table width="100%">
+                            <tr>
+                                <td>
+                                    <SOS/>
+                                </td>
+                                <td>
+                                    <DealerSearch/>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    {/* <Button size="sm" variant="primary" onClick={() => setShow(!showHome)}>Register</Button> */}
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
