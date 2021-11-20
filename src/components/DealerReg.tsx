56fe468528a557d65f1bd932fa96f0ab84110a81
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-21 00:52:11 +0530

    Deleted

--- src/components/DealerReg.tsx
+++ src/components/DealerReg.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { TextField } from '@mui/material';
 
 function DealerReg() {
-    return (
-        <div>
-    
-        </div>
-    );
+	return (
+		<div>
+			<div>
+				<form noValidate autoComplete='off' method='POST'>
+					<h1>PARTNER WITH US</h1>
+					<h1>Enter your details below</h1>
+					<TextField id='dealerName' label='Name' variant='outlined' required />
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default DealerReg;
