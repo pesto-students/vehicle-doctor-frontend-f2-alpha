5044f69154ca3db72be3c3db8335a4ccd8451819
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-25 13:32:32 +0530

    Separated interface and added validations

--- src/components/DealerSignup/FormDealerService.tsx
+++ src/components/DealerSignup/FormDealerService.tsx
@@ -8,39 +8,27 @@ import {
 	MenuItem,
 	SelectChangeEvent,
 	Typography,
-	Button
+	Button,
+	InputAdornment
 } from '@mui/material';
+import { PREVIOUS, NEXT, DEALER_SIGNUP_FORM_HEADER } from '../../Constants/common.constant';
 import axios, { AxiosResponse } from 'axios';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import {
+	IDealerVehService,
+	IServices,
+	IVehicleType,
+	IDealerServiceForm
+} from '../../Interfaces/DealerVehServInterface';
 
-interface Props {
-	nextStep: Function;
-	prevStep: Function;
-	handleFormData: any;
-	values: {
-		vehicletype: {
-			vehicle_type: string;
-		};
-		services: [
-			{
-				service_type: string;
-				cost: number;
-			}
-		];
-	};
-}
-
-interface IServices {
-	id: number;
-	service_type: string;
-	service_name: string;
-}
-
-interface IVehicleType {
-	id: number;
-	vehicle_type: string;
-}
-
-const FormDealerService: React.FC<Props> = ({ nextStep, prevStep, handleFormData, values }) => {
+const FormDealerService: React.FC<IDealerVehService> = ({
+	nextStep,
+	prevStep,
+	handleFormData,
+	values
+}) => {
 	const [vehicleData, setVehicleData] = useState<IVehicleType[]>([]);
 	const [serviceData, setServiceData] = useState<IServices[]>([]);
 
@@ -55,15 +43,41 @@ const FormDealerService: React.FC<Props> = ({ nextStep, prevStep, handleFormData
 		});
 	}, []);
 
+	// Function to continue to next step of the form
 	const Continue = (e: any) => {
 		e.preventDefault();
 		nextStep();
 	};
 
+	// Function to go back to previous step of form
 	const Previous = (e: any) => {
 		e.preventDefault();
 		prevStep();
 	};
+
+	// Yup Validation schema for fields
+	const validationSchema: Yup.SchemaOf<IDealerServiceForm> = Yup.object().shape({
+		vehicletype: Yup.object().shape({
+			vehicle_type: Yup.string().required('Please select a Vehicle Type')
+		}),
+		services: Yup.array().of(
+			Yup.object().shape({
+				service_type: Yup.string().required('Please select a Service Type'),
+				cost: Yup.number().required('Please enter Service Cost')
+			})
+		)
+	});
+
+	//Resolve useForm hook with the validation schema declared above
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors, isValid }
+	} = useForm<IDealerServiceForm>({
+		mode: 'all',
+		resolver: yupResolver(validationSchema)
+	});
 
 	return (
 		<Container component='main' maxWidth='xs'>
@@ -72,47 +86,71 @@ const FormDealerService: React.FC<Props> = ({ nextStep, prevStep, handleFormData
 					<Grid container spacing={2}>
 						<Grid item xs={12}>
 							<Typography component='h1' variant='h5'>
-								Sign Up
+								{DEALER_SIGNUP_FORM_HEADER}
 							</Typography>
 						</Grid>
 
 						<Grid item xs={12}>
 							<InputLabel id='simple-select-standard-label'>Vehicle Type</InputLabel>
-							<Select
-								id='vehicle_type'
-								label='Vehicle Type'
-								variant='standard'
-								onChange={handleFormData('vehicletype.vehicle_type')}
-								fullWidth
-								required>
-								{vehicleData.map((item) => (
-									<MenuItem value={item.id}>{item.vehicle_type}</MenuItem>
-								))}
-							</Select>
+							<Controller
+								name='vehicletype'
+								control={control}
+								render={({ field }) => (
+									<Select
+										id='vehicle_type'
+										label='Vehicle Type'
+										variant='standard'
+										{...field}
+										name='vehicle_type'
+										onChange={handleFormData('vehicletype.vehicle_type')}
+										fullWidth
+										required>
+										{vehicleData.map((item) => (
+											<MenuItem value={item.vehicle_type} key={item.id}>
+												{item.vehicle_type}
+											</MenuItem>
+										))}
+									</Select>
+								)}
+							/>
 						</Grid>
-
-						<Grid item xs={12}>
+						<Grid item xs={12} sm={8}>
 							<InputLabel id='simple-select-standard-label'>Service Type</InputLabel>
-							<Select
-								id='service_type'
-								label='Service Type'
-								variant='standard'
-								onChange={handleFormData('services.service_type')}
-								fullWidth
-								required>
-								{serviceData.map((item) => (
-									<MenuItem value={item.id}>{item.service_type}</MenuItem>
-								))}
-							</Select>
+							<Controller
+								name='services'
+								control={control}
+								render={({ field }) => (
+									<Select
+										id='service_type'
+										label='Service Type'
+										variant='standard'
+										{...field}
+										name='service_type'
+										onChange={handleFormData('services.service_type')}
+										fullWidth
+										required>
+										{serviceData.map((item) => (
+											<MenuItem value={item.service_name} key={item.id}>
+												{item.service_type} - {item.service_name}
+											</MenuItem>
+										))}
+									</Select>
+								)}
+							/>
 						</Grid>
-						<Grid item xs={12}>
+						<Grid item xs={12} sm={4}>
 							<TextField
 								id='cost'
 								label='Cost'
+								{...register('services.0.cost')}
+								name='cost'
 								placeholder='Cost'
 								variant='outlined'
 								defaultValue={values.services[0].cost}
 								onChange={handleFormData('services.cost')}
+								// InputProps={{
+								// 	startAdornment: <InputAdornment position='start'>₹</InputAdornment>,
+								// }}
 								fullWidth
 								required
 							/>
@@ -123,8 +161,8 @@ const FormDealerService: React.FC<Props> = ({ nextStep, prevStep, handleFormData
 								type='submit'
 								fullWidth
 								variant='contained'
-								color='primary'>
-								Previous
+								color='secondary'>
+								{PREVIOUS}
 							</Button>
 						</Grid>
 						<Grid item xs={12} sm={6}>
@@ -134,7 +172,7 @@ const FormDealerService: React.FC<Props> = ({ nextStep, prevStep, handleFormData
 								fullWidth
 								variant='contained'
 								color='primary'>
-								Next
+								{NEXT}
 							</Button>
 						</Grid>
 					</Grid>
